c3cf894c97bf73a5045e9b4fc3d87e8070893660
Olakid645 <108859811+Olakid645@users.noreply.github.com>
2022-07-24 09:04:46 +0100

    Add files via upload
    Uploading a new file

---
+++ SIMPLE INTEREST.py
@@ -0,0 +1,6 @@
+
+principal=float(input("Please enter the principal:"))
+rate=float(input("Please enter the rate:"))
+time=float(input("Please enter the time:"))
+interest=(principal*rate*time)/100
+print(round(interest,2))
